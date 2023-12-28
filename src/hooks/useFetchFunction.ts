3ac3459856fb887fbf08eea0d7fe649585f1b762
SgtMilk <alix.routhier-lalonde@hotmail.com>
2023-12-28 11:08:59 -0500

    updated useFetchFunction

--- src/hooks/useFetchFunction.ts
+++ src/hooks/useFetchFunction.ts
@@ -1,11 +1,10 @@
 import { useState, useRef } from "react";
 
 type FetchFunction = () => Promise<any>;
+type FetchStatus = "loading" | "error" | "ready";
 
 export const useFetchFunction = (fetchFunctions: Array<FetchFunction>) => {
-  const [status, setStatus] = useState<"loading" | "error" | "ready">(
-    "loading"
-  );
+  const [status, setStatus] = useState<FetchStatus>("loading");
   const data = useRef<null | Array<any>>();
 
   Promise.all(fetchFunctions.map((func: FetchFunction) => func()))
